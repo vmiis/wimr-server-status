2ab2516e29526071e9e39fee4d1c44e8de8ab913
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-11-04 18:23:34 +1100

    update

---
+++ modules/module-list.js
@@ -0,0 +1,8 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p=""; //put a unique prefix to avoid conflict with others 
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    var api="wimr";
+    //-------------------------------------------------------------------------------------
+})();
